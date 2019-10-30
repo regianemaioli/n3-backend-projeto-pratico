4a66e4e571d0dd692e7b31408dda3ff06c9e413b
Regiane <regianemaioli@hotmail.com>
2019-10-29 22:27:30 -0300

    inclusão itens concluidos e busca por colaborador

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -12,3 +12,22 @@ exports.getById = (req, res) => {
   }
   res.status(200).send(tarefas.find(tarefas => tarefas.id == id))
 }
+
+exports.getNomeColaborador = (req, res) => {
+  const nome = req.params.nome
+  console.log(nome);
+  //filtrar por nome
+  res.status(200).send(tarefas.filter(tarefas => tarefas.nomeColaborador == nome))
+
+};
+
+  
+exports.getConcluidos = (req, res) => {
+  console.log(req.url);
+  res.status(200).send(
+    tarefas.filter(tarefa => {
+      //retornar as tarefas concluidas
+      return tarefa.concluido === "true";
+    })
+  );
+}
--- src/routes/tarefasRoute.js
+++ src/routes/tarefasRoute.js
@@ -5,6 +5,8 @@ const controller = require("../controllers/tarefasController")
 //criando rotas
 router.get("/", controller.get)
 router.get("/:id", controller.getById)
+router.get("/:nome/buscar", controller.getNomeColaborador)
+router.get("/concluidos/filtrar", controller.getConcluidos)
 
 //tornar a rota disponivel
 module.exports = router
